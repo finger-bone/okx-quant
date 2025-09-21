7106811d9813875329d4c05e11243bbafb19d78d
Zend <ZendWang040302@outlook.com>
2025-09-21 23:48:39 +0800

    init

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+API_KEY = os.getenv("OKX_API_KEY")
+SECRET_KEY = os.getenv("OKX_API_SECRET")
+PASSPHRASE = os.getenv("OKX_API_PASSPHRASE")
+FLAG = os.getenv("OKX_FLAG", "1")  # 1 for demo, 0 for live
+
+SYMBOLS = os.getenv("SYMBOLS", "BTC-USDT,ETH-USDT").split(",")
+WEIGHTS = list(map(float, os.getenv("WEIGHTS", "0.5,0.5").split(",")))
+
+LOOKBACK_DAYS = int(os.getenv("LOOKBACK_DAYS", 365))
+ORDER_USDT_BASE = float(os.getenv("ORDER_USDT_BASE", 100))
+
+import okx.MarketData as MarketData
+
+flag = "1"  # live trading: 0, demo trading: 1
+
+marketDataAPI = MarketData.MarketAPI(flag=flag)
+
+result = marketDataAPI.get_tickers(instType="BTC-USDT")
+print(result)
